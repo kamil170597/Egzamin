e2ceca83ed77d90300b3d3370dcfc83edc780c29
kamil170597 <48652200+kamil170597@users.noreply.github.com>
2019-06-23 15:36:44 +0200

    Update Program.cs

--- Stozek/Slowniki/Program.cs
+++ Stozek/Slowniki/Program.cs
@@ -23,6 +23,58 @@ namespace Slowniki
             {
                 Console.WriteLine($"{zwierze.Key}:{zwierze.Value}");
             }
+            Console.WriteLine();
+
+            string test;
+            Console.WriteLine(Zwierzeta.ContainsKey("Pies"));
+            if(Zwierzeta.TryGetValue("Pies",out test))
+            {
+                Console.WriteLine(test);
+            }
+
+            Console.WriteLine(Zwierzeta.ContainsKey("6"));
+            Zwierzeta.Add("Pająk", "6");
+            foreach (KeyValuePair<string, string> zwierze in Zwierzeta)
+            {
+                Console.WriteLine($"{zwierze.Key}:{zwierze.Value}");
+            }
+            Console.WriteLine();
+
+            foreach (KeyValuePair<string, string> zwierze in Zwierzeta)
+            {
+                Console.WriteLine($"{zwierze.Key}");
+            }
+            Console.WriteLine();
+
+            foreach (KeyValuePair<string, string> zwierze in Zwierzeta)
+            {
+                Console.WriteLine($"{zwierze.Value}");
+            }
+            Console.WriteLine();
+
+            foreach (KeyValuePair<string, string> zwierze in Zwierzeta)
+            {
+                if(zwierze.Value=="4")
+                Console.WriteLine($"{zwierze.Key}:{zwierze.Value}");
+            }
+            Console.WriteLine();
+            int suma = 0;
+            foreach (KeyValuePair<string, string> zwierze in Zwierzeta)
+            {
+                suma = suma + Convert.ToInt32(zwierze.Value);
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Suma nóg:{suma}");
+            Zwierzeta.Remove("Pająk");
+
+            foreach (KeyValuePair<string, string> zwierze in Zwierzeta)
+            {
+               
+                    Console.WriteLine($"{zwierze.Key}:{zwierze.Value}");
+            }
+
+
+
 
         }
     }
