d21442dd626306aa4647ce01c282aa22fe14ec2b
kamil170597 <48652200+kamil170597@users.noreply.github.com>
2019-06-27 16:00:48 +0200

    Dokończyć

---
+++ Stozek/MinMax/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rozmiar,Min=0,Max=0;
+            Random zmienna = new Random();
+            Console.WriteLine("Podaj rozmair tablicy: ");
+            rozmiar=Convert.ToInt32(Console.ReadLine());
+            int[,] tab = new int[rozmiar,rozmiar];
+
+            for (int i = 0; i < rozmiar; i++)
+            {
+                for (int j = 0; j < rozmiar; j++)
+                {
+                    tab[i, j] = zmienna.Next(1, 100);
+                }
+            }
+
+            for (int i = 0; i < rozmiar; i++)
+            {
+                Console.WriteLine("\n");
+                for (int j = 0; j < rozmiar; j++)
+                {
+                    Console.Write($"{tab[i, j]} ");
+
+                }
+
+            }
+            Console.WriteLine("");
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
